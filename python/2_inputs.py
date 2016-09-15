99635911c82cc7761aff30a58b300226932a6b54
lior israeli <lisrael1@cs.huji.ac.il>
2016-09-15 10:32:43 +0300

    fix 2_inputs.py to normal

--- python/2_inputs.py
+++ python/2_inputs.py
@@ -1,71 +1,26 @@
 #!/usr/bin/python
 
 #next TODO:
-#try to draw mse vs bin sizes here to see that this code is as the last one
 #try to do SNR minus quantization SNR
 #add different modulo and quantization also on y
-
+#add simple and short flow for code sanity check
+#change max val to sigma and change uniform to normal!!!!!!!!!
 
 
 """
+we have here only 2 inputs, y and x, and each input has its own modulo size and quantization
+we want to see how the change of y modulo and quantization replects on x mse
+
 gvim shortcuts:
 zf create folding
 za toggle folding state
-system goes like this:
-basic system:
-	encoder:
-		modulo input x
-			modulo size by var(x-y)
-	decoder 1:
-		multiply [x,y] in A 
-		modulo
-	decoder 2:
-		multiply at the inverse of A
-full system:
-	in equations:
-		mod(   q( mod( ax + D ))    -D - ay)
-			a is alpha
-		q(ax+D)=ax+D+z   
-			z=quantization error
-		so we will get the delta between x and y is:
-			ax+D+z-D-ay
-			=a(x-y+z)
-		and for recovering x we will just do delta+y
-	in flow:
-		input generator:
-			by cov matrix
-		encoder 
-			multipy by alpha
-			add dither
-			modulo
-			quantisize
-		the decoder has 2 blocks:
-		decoder 1:
-			subtitude by dither 
-			subtitude by alpha y 
-				or any other integer combinations - multiply by A (here its [1,-1]) so it will be inside the modulo
-				to get linear algebra we probably better use A=[[1,-1],[-1,1]]
-			modulo
-			multiply by alpha
-
-			here we actually get mod(x-y) and we hope that it's inside the modulo
-		decoder 2:
-			add y (the inverse of A)
-
-
-alpha is var(x-y) / [var(x-y)+var(dither)] - wielner coefficient
-
-opens:
-	modulo depends on the statistice - on the cov so it might be that we dont have the same statistics but we want a known modulo size ahead so we want same modulo size to all
-	so all inputs have the same modulo size, number of bins
-	but what about var and alpha?
 	
 """
 from time import time
 start_time = time()
 from numpy import matrix as m
 from numpy import *
-from pylab import plot,show,grid,xlabel,ylabel,title,close,savefig,ion,draw,figure
+from pylab import plot,show,grid,xlabel,ylabel,title,legend,close,savefig,ion,draw,figure
 from time import sleep
 from multiprocessing import Pool
 from operator import methodcaller
@@ -73,16 +28,15 @@ set_printoptions(precision=6, threshold=None, edgeitems=None, linewidth=100, sup
 
 
 
-#if the modulo_size here for example is 1.5, we will 
+#if the modulo_size here for example is 3, we will 
 #not change values between -1.5 and 1.5, but 1.6 will become -1.4
 def mod(num,modulo_size):
-    return m((m(num)+modulo_size)%(2*modulo_size)-modulo_size)
-
+	return m((m(num)+modulo_size/2.0)%(modulo_size)-modulo_size/2.0)
 
 #each row is at specific time, each column is an input
-def generate_data(covar,max_val,inputs,samples):
+def generate_data(covar,var,inputs,samples):
 	#fist input will be the uniform dist and the others will be the normal around it:
-	data=m([hstack((i,random.normal(i,sqrt(covar),inputs-1))) for i in random.uniform(-max_val,max_val,samples)])
+	data=m([hstack((i,random.normal(i,sqrt(covar),inputs-1))) for i in random.normal(0,sqrt(var),samples)])
 	return data
 def add_dither(data,dither_size):
 	rows,columns=data.shape
@@ -95,23 +49,25 @@ def add_dither(data,dither_size):
 
 #this function quantize a number by a given quantizer with bins
 def quantizise(mumbers,quant_size,number_of_quants):
-	#rounding to the quantizer
-	q=rint(mumbers/quant_size)*quant_size
 	#taking max and min values to be at the last bins
-	max_val=quant_size*number_of_quants
+	max_val=quant_size*number_of_quants/2.0
+	#rounding to the quantizer
+	q=rint((mumbers+max_val)/quant_size)*quant_size-max_val
 	q=mat([max_val if i>max_val else -max_val if i<-max_val else i for i in q.A1]).reshape(q.shape)
 	return q
 
 
 class data:
 	#when you create data, it will run it and calculate the dither, the data after modulo and after all decoders..
-	def __init__(self,number_of_inputs,number_of_samples,data_max_val,covar,mod_size,num_quants,dither_on):
-		self.number_of_inputs=number_of_inputs
+	def __init__(self,number_of_samples,var,covar,x_mod_size,num_quants_for_x,y_mod_size,num_quants_for_y,dither_on):
+		self.number_of_inputs=2
 		self.number_of_samples=number_of_samples
-		self.data_max_val=data_max_val
+		self.var=var
 		self.covar=covar
-		self.mod_size=mod_size
-		self.num_quants=num_quants
+		self.x_mod_size=x_mod_size
+		self.y_mod_size=y_mod_size
+		self.num_quants_for_x=num_quants_for_x
+		self.num_quants_for_y=num_quants_for_y
 		self.dither_on=dither_on
 		self.init_calculations()
 	def __str__(self):
@@ -125,8 +81,10 @@ class data:
 		print "===variables==="
 		print "number of inputs\n",self.number_of_inputs
 		print "number of samples\n",self.number_of_samples
-		print "modulo size\n",self.mod_size
-		print "number of quants\n",self.num_quants
+		print "x modulo size\n",self.x_mod_size
+		print "y modulo size\n",self.y_mod_size
+		print "number of x quants\n",self.num_quants_for_x
+		print "number of y quants\n",self.num_quants_for_y
 		print "covar\n",self.covar
 		print "dither size\n",self.dither_size
 		return self
@@ -134,9 +92,12 @@ class data:
 		print "===data==="
 		print "original_data\n",self.original_data
 		print "y x_original delta (y is the first input):\n",self.original_data-self.original_data[:,0]
-		print "after_dither\n",self.after_dither
+		print "x_after_dither\n",self.x_after_dither
 		print "x_after_modulo\n",self.x_after_modulo
+		print "y_after_modulo\n",self.y_after_modulo
 		print "x_after_quantizer\n",self.x_after_quantizer
+		print "y_after_quantizer\n",self.y_after_quantizer
+		print "modulo on x_y_delta after quantizers\n",self.x_y_delta
 		print "recovered_x\n",self.recovered_x
 		print "error\n",self.error
 		print "mse\n",m(self.mse_per_input_sample)
@@ -144,48 +105,38 @@ class data:
 		print "-------------------------------------------"
 		return self
 	def init_calculations(self):
-		self.quant_size=1.0*self.mod_size/(self.num_quants+1)
+		self.x_quant_size=1.0*self.x_mod_size/self.num_quants_for_x
+		self.y_quant_size=1.0*self.y_mod_size/self.num_quants_for_y
 		self.dither_size=0
 		if (self.dither_on):
-			self.dither_size=self.quant_size
+			self.dither_size=self.x_quant_size
 	def finish_calculations(self):
 		#we need to remove the first column because we get its original values
 		self.error=self.original_x-self.recovered_x
 		#for mse we will flaten the error matrix so we can do power 2 just by dot product
 		self.mse_per_input_sample=sum(self.error.A1.T*self.error.A1)/((self.number_of_inputs-1)*self.number_of_samples)
-		#i think i can remove this... self.all_data_var=var(self.original_x) #should be (2*data_max_val)^2/12
+		#i think i can remove this... self.all_data_var=var(self.original_x) #should be (2*var)^2/12
 		#what should impact on the mse is the number of inputs, samples modulo size and covar but not on the single data variance
 		#i think i can remove this... 	self.normal_mse=(self.mse_per_input_sample/self.covar)/((self.number_of_inputs-1)*self.number_of_samples)#not working...
-		self.snr=(4*self.data_max_val*self.data_max_val)/self.mse_per_input_sample
+		self.snr=(4*self.var*self.var)/self.mse_per_input_sample
 		self.capacity=log2(self.snr+1)
 	#this function is for only 2 inputs
 	def run_sim(self):
-		#TODO some how i ruind this part and 
-		self.original_data=generate_data(self.covar,self.data_max_val,self.number_of_inputs,self.number_of_samples)
-		self.original_x=self.original_data[:,1:]
+		self.original_data=generate_data(self.covar,self.var,self.number_of_inputs,self.number_of_samples)
+		self.original_x=self.original_data[:,1] #[:,1:]
 		self.original_y=self.original_data[:,0]
-		self.after_dither,self.dither=add_dither(self.original_x,self.dither_size)
-		self.x_after_modulo=mod(self.after_dither,self.mod_size)
-		self.x_after_quantizer=quantizise(self.x_after_modulo,self.quant_size,self.num_quants)-self.dither
+		self.x_after_dither,self.dither=add_dither(self.original_x,self.dither_size)
+		self.x_after_modulo=mod(self.x_after_dither,self.x_mod_size)
+		self.x_after_quantizer=quantizise(self.x_after_modulo,self.x_quant_size,self.num_quants_for_x)-self.dither
+		self.y_after_dither=self.original_y+self.dither
+		self.y_after_modulo=mod(self.y_after_dither,self.y_mod_size)
+		self.y_after_quantizer=quantizise(self.y_after_modulo,self.y_quant_size,self.num_quants_for_y)-self.dither
 		#TODO alpha
 		#TODO modulo and quantizer also on y, but not the same modulo and quantizer of x
 		#aqctually we pick here x-y but it should be multiply in A
-		self.x_y_delta=mod(self.x_after_quantizer-self.original_y,self.mod_size)
-		self.recovered_x=self.x_y_delta+self.original_y
+		self.x_y_delta=mod(self.x_after_quantizer-self.y_after_quantizer,self.x_mod_size)
+		self.recovered_x=self.x_y_delta+self.y_after_quantizer
 		self.finish_calculations()
-
-
-#preparing the data:
-d=[data(
-	number_of_inputs=2,#input*samples: 300*40 will take 27 sec, 300*400 4 minutes, and 300*4000 will take 40 minutes, 300*4e4 will get memory error
-	number_of_samples=400,
-	data_max_val=10,
-	covar=1,
-	mod_size=i,
-	num_quants=j,
-	dither_on=0
-	) for i in arange(0.1,16,.05) for j in range(1,40)]
-
 
 #a function for running parallel:
 def n(a):
@@ -193,12 +144,54 @@ def n(a):
 	return a
 
 
-#parsing output:
+
+
+
+
+
+
+
+
+
+
+
+
+#this one is just a short simulation to check if the flow ok:
+if (0):
+	d_for_prints=[data(
+		#input*samples: 300*40 will take 27 sec, 300*400 4 minutes, and 300*4000 will take 40 minutes, 300*4e4 will get memory error
+		number_of_samples=3,
+		var=10,
+		covar=1,
+		x_mod_size=i,
+		num_quants_for_x=j,
+		y_mod_size=24,
+		num_quants_for_y=300,
+		dither_on=0
+		) for i in [1,4,8] for j in [1,4,300]]
+	d=Pool().imap_unordered(n,d_for_prints)
+	#d=map(n,d)
+	[i.print_all() for i in d]
+
+
+
+#preparing the data:
+d=[data(
+	#input*samples: 300*40 will take 27 sec, 300*400 4 minutes, and 300*4000 will take 40 minutes, 300*4e4 will get memory error
+	number_of_samples=400,
+	var=10,
+	covar=1,
+	x_mod_size=i,
+	num_quants_for_x=j,
+	y_mod_size=24,
+	num_quants_for_y=3,
+	dither_on=0
+	) for i in arange(0.1,16,.05) for j in range(1,40)]
 
 #running on each number of quants:
 if (0):
 	d=Pool().imap_unordered(n,d)
-	o=m([[i.mod_size,i.mse_per_input_sample,i.num_quants] for i in d])
+	o=m([[i.x_mod_size,i.mse_per_input_sample,i.num_quants_for_x] for i in d])
 	for j in set(o[:,2].A1):
 		j=int(j)
 		print j
@@ -214,11 +207,11 @@ if (0):
 
 
 #running on best mse for each:
-if (1):
+if (0):
 	d=Pool().imap_unordered(n,d)
 	#d=map(n,d)
-	o=[[i.mse_per_input_sample,i.num_quants] for i in d]
-	#o=[[i.capacity,i.num_quants] for i in d]
+	o=[[i.mse_per_input_sample,i.num_quants_for_x] for i in d]
+	#o=[[i.capacity,i.num_quants_for_x] for i in d]
 	#now we will take only one line per number of bins:
 	o=sorted(o,key=lambda e:e[0], reverse=True)
 	o={i[1]:[i[0],i[1]] for i in o}.values()
@@ -228,34 +221,43 @@ if (1):
 	plot(o[:,1],o[:,0])
 	xlabel("bins")
 	ylabel("mse")
-	title("best mse per bins ")
+	title("best mse per bins")
 	grid()
 	print "simulation time: ",time() - start_time,"sec"
 	show()
 
 #this will loop on options 
-if (0):
-	for k in [10,20,30]:
+if (1):
+	for k in [10,41,50]:
 		d=[data(
-			number_of_inputs=2,#input*samples: 300*40 will take 27 sec, 300*400 4 minutes, and 300*4000 will take 40 minutes, 300*4e4 will get memory error
 			number_of_samples=40,
-			data_max_val=k,
+	 		var=k,
 			covar=1,
-			mod_size=i,
-			num_quants=j,
+			x_mod_size=i,
+			num_quants_for_x=j,	
+			y_mod_size=k*2.5,
+			num_quants_for_y=15,
 			dither_on=0
-			) for i in arange(0.1,16,.05) for j in range(1,40)]
+			) 
+				#for i in arange(0.1,16,.05) for j in range(1,40)
+				for i in [8] for j in range(1,40)
+			]
 		d=Pool().imap_unordered(n,d)
 		#d=map(n,d)
-		#o=[[i.mod_size,i.capacity,i.num_quants] for i in d]
-		o=[[i.mod_size,i.snr,i.num_quants] for i in d]
-		#o=[[i.mod_size,i.mse_per_input_sample,i.num_quants] for i in d]
+		#o=[[i.x_mod_size,i.capacity,i.num_quants_for_x] for i in d]
+		#o=[[i.x_mod_size,i.snr,i.num_quants_for_x] for i in d]
+		o=[[i.x_mod_size,i.mse_per_input_sample,i.num_quants_for_x] for i in d]
+		print m(o)
 		#now we will take only one line per number of bins:
 		o=sorted(o,key=lambda e:e[1], reverse=True)
 		o={i[2]:[i[0],i[1],i[2]] for i in o}.values()
 		o=m(o)
-		plot(o[:,2],o[:,1])
+		plot(o[:,2],o[:,1],label=k)
+	xlabel("bins")
+	ylabel("mse")
+	title("best mse per bins")
 	grid()
+	legend(loc="best")
 	print "simulation time: ",time() - start_time,"sec"
 	show()
 print "simulation time: ",time() - start_time,"sec"

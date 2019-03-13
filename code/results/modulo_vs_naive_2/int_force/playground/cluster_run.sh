42c0cc28cf7b660ea368ca4a21905a5e7b18fc0d
lior israeli <lisrael1@cs.huji.ac.il>
2019-03-13 09:09:03 +0200

    updatging cluster command with dependency

--- code/results/modulo_vs_naive_2/int_force/playground/cluster_run.sh
+++ code/results/modulo_vs_naive_2/int_force/playground/cluster_run.sh
@@ -1,1 +1,11 @@
+high bins and snr
+sbatch --mem=1800m -c1 --time=23:50:0 --array=0-1 --wrap 'python3 flow_cross_methods.py -s ${SLURM_JOB_ID}_${SLURM_ARRAY_TASK_ID} --methods="[\"basic_method\"]" -q "[0,0.025,150]" -b "[1024,5001]" --snr="[10000]" -m 1'
+sbatch --mem=1800m -c1 --time=23:50:0 --array=0-499 --wrap 'python3 flow_cross_methods.py -s ${SLURM_JOB_ID}_${SLURM_ARRAY_TASK_ID} --methods="[\"clipping_method\"]" -q "[0,0.025,150]" -b "[1024,5001]" --snr="[10000]" -m 2'
 sbatch --mem=1800m -c1 --time=23:50:0 --array=0-499 --wrap 'python3 flow_cross_methods.py -s ${SLURM_JOB_ID}_${SLURM_ARRAY_TASK_ID} --methods="[\"sinogram_method\"]" -q "[0,0.025,150]" -b "[1024,5001]" --snr="[1000001,10000000]" -m 2'
+
+
+sbatch --mem=1800m -c1 --time=23:50:0 --array=0-1 --wrap 'python3 flow_cross_methods.py -s ${SLURM_JOB_ID}_${SLURM_ARRAY_TASK_ID} --methods="[\"basic_method\"]" -q "[0,0.5,150]" -b "[101,511]" --snr="[100000]" -m 1'
+sbatch --mem=1800m -c1 --time=23:50:0 --array=0-499 --wrap 'python3 flow_cross_methods.py -s ${SLURM_JOB_ID}_${SLURM_ARRAY_TASK_ID} --methods="[\"clipping_method\"]" -q "[0,0.5,150]" -b "[101,511]" --snr="[100000]" -m 2'
+sbatch --mem=1800m -c1 --time=23:50:0 --array=0-499 --wrap 'python3 flow_cross_methods.py -s ${SLURM_JOB_ID}_${SLURM_ARRAY_TASK_ID} --methods="[\"sinogram_method\"]" -q "[0,0.5,150]" -b "[101,511]" --snr="[100000]" -m 2'
+
+sbatch --dependency=singleton --wrap 'python3 plot_res.py;cp pivot.csv *.html ~/www;date|mutt -s done israelilior@gmail.com'
--- code/results/modulo_vs_naive_2/int_force/playground/runall.sh
+++ code/results/modulo_vs_naive_2/int_force/playground/runall.sh
@@ -1,5 +1,14 @@
 # run me with nohup bash runall.sh&
-sleep 70000
+# but now you have it at the cluster command, using dependency...
+sleep 10000
+python3 plot_res.py 
+cp pivot.csv *.html ~/www
+date|mutt -s done israelilior@gmail.com
+sleep 10000
+python3 plot_res.py 
+cp pivot.csv *.html ~/www
+date|mutt -s done israelilior@gmail.com
+sleep 10000
 python3 plot_res.py 
 cp pivot.csv *.html ~/www
 date|mutt -s done israelilior@gmail.com

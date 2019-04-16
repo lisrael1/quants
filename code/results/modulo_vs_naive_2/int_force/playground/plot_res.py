cdfd5146369bd02ee3a0f02add12d666cd0c082c
lisrael1 <israelilior@gmail.com>
2019-04-16 22:39:33 +0300

    adding plots per number of samples

--- code/results/modulo_vs_naive_2/int_force/playground/plot_res.py
+++ code/results/modulo_vs_naive_2/int_force/playground/plot_res.py
@@ -1,54 +1,48 @@
 if __name__ == '__main__':
 	import pandas as pd
 	import numpy as np
-	# import pdb
-	# from IPython.core.debugger import set_trace
-	# pdb.set_trace()
-	# pd.set_option("display.max_rows",1000)
 	pd.set_option("display.max_columns",1000)
 	pd.set_option('expand_frame_repr',False)
 	pd.set_option('display.max_colwidth',1000)
 	import plotly as py
 	import cufflinks
-	# import scipy.signal as signal
 	from glob import glob
 	from tqdm import tqdm
 
-	number_of_samples_at_scatter_plot=50000
-	
+
 	df=pd.DataFrame()
 	alls=[]
 	#for f in tqdm(glob(r"*gz")[:10]):
 	for f in tqdm(glob(r"*gz")):
 		tmp=pd.read_csv(f)#, nrows=200000)
 		if 0:
-		    df=pd.concat([df,tmp], axis=0, sort=True)
+			df=pd.concat([df,tmp], axis=0, sort=True)
 		if 0:
-		    df=df.append(tmp,sort=True)
+			df=df.append(tmp,sort=True)
 		if 1:
-		    alls+=[tmp]
+			alls+=[tmp]
 	print('now doint concatenation')
 	df=pd.concat(alls, ignore_index=True)
-	#df.loc[df.rmse.isna(),'rmse']=df.loc[df.rmse.isna()].mse**0.5
-	#df.head()
-	
+	number_of_samples_at_scatter_plot = 50000
+
 	print('doing pivot')
-	pivot=df.pivot_table(columns=['snr','number_of_bins','method'], index='quant_size', values='rmse', aggfunc=['mean', 'std'])
+	pivot=df.pivot_table(columns=['snr','number_of_bins','method','samples'], index='quant_size', values='rmse', aggfunc=['mean', 'std'])
 	pivot.to_csv('pivot.csv')
 
 	print('plot rmse')
+
 	def remove_nans_from_plot(fig):
 		for trace in range(len(fig['data'])):
 			inx = np.where(fig['data'][trace]['y'] == '')
 			fig['data'][trace]['x'] = np.delete(fig['data'][trace]['x'], inx)
 			fig['data'][trace]['y'] = np.delete(fig['data'][trace]['y'], inx)
 		return fig
-	fig=pivot['mean'].figure(xTitle='quant_size', yTitle='mean', title='rmse per quant sizee, by snr, number of binds and methods')
+	fig=pivot['mean'].figure(xTitle='quant_size', yTitle='mean', title='rmse per quant size, by snr, number of binds and methods')
 	fig=remove_nans_from_plot(fig)
 	py.offline.plot(fig, filename='plot rmse per quant size snr number of bins and method.html', auto_open=False)
 
 	print('plot std')
-	fig = pivot['std'].figure(xTitle='quant_size', yTitle='std', title='std per quant sizee, by snr, number of binds and methods')
+	fig = pivot['std'].figure(xTitle='quant_size', yTitle='std', title='std per quant size, by snr, number of binds and methods')
 	py.offline.plot(fig, filename='plot std per quant size snr number of bins and method.html', auto_open=False)
 
 	print('find cov angle')
@@ -59,8 +53,6 @@ if __name__ == '__main__':
 		from tqdm import tqdm
 		tqdm.pandas()
 		df['angle'] = df[df.method=='sinogram_method'].sample(100000).progress_apply(lambda row: int_force.rand_data.find_slop.get_cov_ev(eval(row['cov']))[1], axis=1)
-	fig=df[~df.angle.isna()].set_index('angle').rmse.sample(number_of_samples_at_scatter_plot).figure(kind='scatter', mode='markers', size=4, opacity=0.1, title='rmse per angle')
-	py.offline.plot(fig, filename='plot rmse per angle.html', auto_open=False)
 
 	def redo_hist_on_hist(hist, number_of_bins = 150):
 		'''
@@ -82,36 +74,14 @@ if __name__ == '__main__':
 	print('plot angle error')
 	if 'angle' in df.columns:
 		df['angle_error']=df.angle-df.cov_ev_angle
-		fig=df.set_index('cov_ev_angle').angle_error.dropna().sample(number_of_samples_at_scatter_plot).figure(kind='scatter', mode='markers', size=4, title='angle error per angle')
-		py.offline.plot(fig, filename='plot angle error per angle.html', auto_open=False)
-		
-		fig=df.dropna(subset=['angle']).set_index('cov_ev_angle').rmse.sample(number_of_samples_at_scatter_plot).figure(kind='scatter', mode='markers', size=4, title='rmse per angle')
-		py.offline.plot(fig, filename='plot rmse per angle.html', auto_open=False)
-		
-		fig=df.set_index('angle_error').rmse.dropna().sample(number_of_samples_at_scatter_plot).figure(kind='scatter', mode='markers', size=4, title='rmse per angle error')
-		py.offline.plot(fig, filename='plot rmse per angle error.html', auto_open=False)
+		for x, y in [['cov_ev_angle', 'angle_error'], ['cov_ev_angle', 'rmse'], ['angle_error', 'rmse']]:
+			fig = df.dropna(subset=['angle']).pivot_table(index=x, columns='samples', values=y).stack().to_frame(y).reset_index(drop=False).astype(str).figure(
+				kind='scatter', x=x, y=y, categories='samples', size=7, opacity=0.9, title='%s per %s'%(y, x))
+			py.offline.plot(fig, filename='plot %s per %s.html'%(y, x), auto_open=False)
 
 	print('plot dist')
 	df['legend'] = df.method + ":" + df.snr.astype(str) + ':' + df.number_of_bins.astype(str)
-	if 1:
-		fig = df[df.legend.str.endswith(':10000:101')]
-		if not fig.empty:
-			fig = fig.sample(10000).figure(kind='scatter', x='quant_size', y='rmse', categories='legend', size=4, opacity=0.2, title='all samples per quant sizee, by snr, number of binds and methods')
-			py.offline.plot(fig, filename='plot all samples per quant size snr number of bins and method.html', auto_open=False)
-	else:
-		fig = df.figure(kind='scatter', x='quant_size', y='rmse', categories='legend', size=1, opacity=0.01, title='all samples per quant sizee, by snr, number of binds and methods')
-		for i in range(len(fig['data'])): fig['data'][i]['visible'] = 'legendonly'
+	fig = df[df.legend.str.endswith(':10000:101')]
+	if not fig.empty:
+		fig = fig.sample(10000).figure(kind='scatter', x='quant_size', y='rmse', categories='legend', size=4, opacity=0.2, title='all samples per quant size, by snr, number of binds and methods')
 		py.offline.plot(fig, filename='plot all samples per quant size snr number of bins and method.html', auto_open=False)
-
-	exit()
-	
-	
-	
-	
-	
-	best_rmse=pivot.min().unstack(2).unstack(0)
-	
-	a=best_rmse
-	print(a.modulo_method/a.clipping_method)
-	a=best_rmse
-	print(a.ml_method/a.clipping_method)

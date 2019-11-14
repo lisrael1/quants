7450d1fdc0b6db3c608b0651f2049a618e49caa0
lisrael1 <lisrael1@cs.huji.ac.il>
2019-11-14 22:18:09 +0200

    little fixes

--- code/results/modulo_vs_naive_2/int_force/global_imports.py
+++ code/results/modulo_vs_naive_2/int_force/global_imports.py
@@ -8,10 +8,10 @@ class timer:
         import time
         self.time=time
         self.start = time.time()
-
     def current(self):
         return "{:} sec".format(self.time.time() - self.start)
-
     def __del__(self):
         print("closing timer with value {:}".format(self.current()))
-        return self.current()
+        return self.current()  
+    def __exit__(self, exception_type, exception_value, traceback): pass # needed for using at 'with' command
+    def __enter__(self): pass
